37501f03c4104a8b82af53ef4481d7064160b4a1
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-19 13:55:25 -0700

    Implement additional product integration tests

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -12,4 +12,61 @@ describe "the add a product process" do
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Spicy Beans'
   end
+
+  it "remains on the page when a field is empty" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).not_to have_content 'Product successfully added!'
+  end
+end
+
+describe "the edit a product process" do
+  it "edits a product" do
+    visit root_path
+    click_link 'Head to our product catalogue.'
+    click_link 'Add a new product'
+    fill_in 'Product Name', :with => 'spicy beans'
+    fill_in 'Product Price', :with => '3.50'
+    fill_in 'Product Origin', :with => 'Brazil'
+    click_on 'Create Product'
+    click_on 'Spicy Beans'
+    click_on 'Edit or delete product'
+    fill_in 'Product Price', :with => '2.75'
+    click_on 'Update Product'
+    expect(page).to have_content "Product successfully updated!"
+  end
+
+  it "remains on the page when a field is empty" do
+    visit root_path
+    click_link 'Head to our product catalogue.'
+    click_link 'Add a new product'
+    fill_in 'Product Name', :with => 'spicy beans'
+    fill_in 'Product Price', :with => '3.50'
+    fill_in 'Product Origin', :with => 'Brazil'
+    click_on 'Create Product'
+    click_on 'Spicy Beans'
+    click_on 'Edit or delete product'
+    fill_in 'Product Name', :with => ''
+    fill_in 'Product Price', :with => ''
+    fill_in 'Product Origin', :with => ''
+    click_on 'Update Product'
+    expect(page).not_to have_content "Product successfully updated!"
+  end
+
+
+  describe "the delete product process" do
+    it "deletes a product" do
+      visit root_path
+      click_link 'Head to our product catalogue.'
+      click_link 'Add a new product'
+      fill_in 'Product Name', :with => 'spicy beans'
+      fill_in 'Product Price', :with => '3.50'
+      fill_in 'Product Origin', :with => 'Brazil'
+      click_on 'Create Product'
+      click_on 'Spicy Beans'
+      click_on 'Edit or delete product'
+      click_on 'Delete Product'
+      expect(page).not_to have_content "Spicy Beans"
+    end
+  end
 end
