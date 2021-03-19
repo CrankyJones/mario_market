f64851d720b4c3404b649b1e8d98ba4935bea7d0
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-19 09:46:39 -0700

    Add prduct/review specs/validations with passing tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
-  
+  belongs_to :product
+  validates :author, presence: true 
+  validates_length_of  :content_body, minimum: 50, maximum: 250
+  validates_inclusion_of :rating, in: 1..5
 end
