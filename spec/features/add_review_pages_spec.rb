9f7773e4ab1f5ca57f96dcc730879bac4951466c
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-19 15:45:07 -0700

    Add passing good fail reviews integration tests

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "the add a review process" do
-  it "adds a new product" do
+  it "adds a new review" do
     visit root_path
     click_link 'Head to our product catalogue.'
     click_link 'Add a new product'
@@ -19,10 +19,24 @@ describe "the add a review process" do
     expect(page).to have_content 'Review successfully added!'
     expect(page).to have_content 'Great Product'
   end
+
+  it "add a new review fail" do
+    visit root_path
+    click_link 'Head to our product catalogue.'
+    click_link 'Add a new product'
+    fill_in 'Product Name', :with => 'spicy beans'
+    fill_in 'Product Price', :with => '3.50'
+    fill_in 'Product Origin', :with => 'Brazil'
+    click_on 'Create Product'
+    click_on 'Spicy Beans'
+    click_on 'Add a review for this product'
+    click_on 'Create Review'
+    expect(page).not_to have_content 'Review successfully added!'
+  end
 end
 
 describe "the edit a review process" do
-  it "edits a product" do
+  it "edits a review" do
     visit root_path
     click_link 'Head to our product catalogue.'
     click_link 'Add a new product'
@@ -42,6 +56,29 @@ describe "the edit a review process" do
     fill_in 'Enter a title for your review:', :with => 'Bad Product'
     click_on 'Update Review'
     expect(page).to have_content "Review successfully updated!"
+  end
+  it "edits a review fail" do
+    visit root_path
+    click_link 'Head to our product catalogue.'
+    click_link 'Add a new product'
+    fill_in 'Product Name', :with => 'spicy beans'
+    fill_in 'Product Price', :with => '3.50'
+    fill_in 'Product Origin', :with => 'Brazil'
+    click_on 'Create Product'
+    click_on 'Spicy Beans'
+    click_on 'Add a review for this product'
+    fill_in 'Enter your name:', :with => 'Sally Robinson'
+    fill_in 'Enter a title for your review:', :with => 'Great Product'
+    fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => 'this is a lot of characterssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
+    fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => '1'
+    click_on 'Create Review'
+    click_on 'Great Product'
+    click_on 'Edit or Delete Review'
+    fill_in 'Enter a title for your review:', :with => ''
+    fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => ''
+    fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => ''
+    click_on 'Update Review'
+    expect(page).not_to have_content "Review successfully updated!"
   end
 end
 
