bae9d3aef8f561ed265097a30f9baf026736c869
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-19 14:56:49 -0700

    Successfully seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
+require 'faker'
+
 Product.destroy_all
 Review.destroy_all
 
-50.times do |t|
-  product = Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+10.times do |t|
+  product = Product.create!(name: Faker::Appliance.unique.equipment, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
   5.times do |t|
-    review = Review.create!(title: Faker::Lorem.words(number: 3) Faker::, author: Faker::FunnyName.name, content_body: Faker::Lorem.words(number: 15))
+    review = product.reviews.create!(title: Faker::Lorem.words(number: 3), author: Faker::FunnyName.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.words(number: 15))
   end
 end
+
+20.times do |t|
+  product = Product.create!(name: Faker::Beer.unique.brand, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+  5.times do |t|
+    review = product.reviews.create!(title: Faker::Lorem.words(number: 3), author: Faker::FunnyName.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.words(number: 15))
+  end
+end
+
+20.times do |t|
+  product = Product.create!(name: Faker::Food.unique.ingredient, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country)
+  5.times do |t|
+    review = product.reviews.create!(title: Faker::Lorem.words(number: 3), author: Faker::FunnyName.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.words(number: 15))
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
