0444732ddf5dadb133d9443b11155674efb32774
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-24 17:36:07 -0700

    Add buttons to access product page and correct specs

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -4,7 +4,7 @@ describe "the add a product process" do
   it "adds a new product" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_on 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
@@ -24,32 +24,32 @@ describe "the edit a product process" do
   it "edits a product" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Edit or delete product'
+    click_button 'Edit or delete product'
     fill_in 'Product Price', :with => '2.75'
-    click_on 'Update Product'
+    click_button 'Update Product'
     expect(page).to have_content "Product successfully updated!"
   end
 
   it "remains on the page when a field is empty" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Edit or delete product'
+    click_button 'Edit or delete product'
     fill_in 'Product Name', :with => ''
     fill_in 'Product Price', :with => ''
     fill_in 'Product Origin', :with => ''
-    click_on 'Update Product'
+    click_button 'Update Product'
     expect(page).not_to have_content "Product successfully updated!"
   end
 
@@ -58,14 +58,14 @@ describe "the edit a product process" do
     it "deletes a product" do
       visit root_path
       click_link 'Head to our product catalogue.'
-      click_link 'Add a new product'
+      click_button 'Add a new product'
       fill_in 'Product Name', :with => 'spicy beans'
       fill_in 'Product Price', :with => '3.50'
       fill_in 'Product Origin', :with => 'Brazil'
-      click_on 'Create Product'
+      click_button 'Create Product'
       click_on 'Spicy Beans'
-      click_on 'Edit or delete product'
-      click_on 'Delete Product'
+      click_button 'Edit or delete product'
+      click_link 'Delete Product'
       expect(page).not_to have_content "Spicy Beans"
     end
   end
--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -4,18 +4,18 @@ describe "the add a review process" do
   it "adds a new review" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Add a review for this product'
+    click_button 'Add a review for this product'
     fill_in 'Enter your name:', :with => 'Sally Robinson'
     fill_in 'Enter a title for your review:', :with => 'Great Product'
     fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => 'this is a lot of characterssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
     fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => '1'
-    click_on 'Create Review'
+    click_button 'Create Review'
     expect(page).to have_content 'Review successfully added!'
     expect(page).to have_content 'Great Product'
   end
@@ -23,14 +23,14 @@ describe "the add a review process" do
   it "add a new review fail" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Add a review for this product'
-    click_on 'Create Review'
+    click_button 'Add a review for this product'
+    click_button 'Create Review'
     expect(page).not_to have_content 'Review successfully added!'
   end
 end
@@ -39,45 +39,45 @@ describe "the edit a review process" do
   it "edits a review" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Add a review for this product'
+    click_button 'Add a review for this product'
     fill_in 'Enter your name:', :with => 'Sally Robinson'
     fill_in 'Enter a title for your review:', :with => 'Great Product'
     fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => 'this is a lot of characterssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
     fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => '1'
-    click_on 'Create Review'
+    click_button 'Create Review'
     click_on 'Great Product'
-    click_on 'Edit or Delete Review'
+    click_button 'Edit or Delete Review'
     fill_in 'Enter a title for your review:', :with => 'Bad Product'
-    click_on 'Update Review'
+    click_button 'Update Review'
     expect(page).to have_content "Review successfully updated!"
   end
   it "edits a review fail" do
     visit root_path
     click_link 'Head to our product catalogue.'
-    click_link 'Add a new product'
+    click_button 'Add a new product'
     fill_in 'Product Name', :with => 'spicy beans'
     fill_in 'Product Price', :with => '3.50'
     fill_in 'Product Origin', :with => 'Brazil'
-    click_on 'Create Product'
+    click_button 'Create Product'
     click_on 'Spicy Beans'
-    click_on 'Add a review for this product'
+    click_button 'Add a review for this product'
     fill_in 'Enter your name:', :with => 'Sally Robinson'
     fill_in 'Enter a title for your review:', :with => 'Great Product'
     fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => 'this is a lot of characterssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
     fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => '1'
-    click_on 'Create Review'
+    click_button 'Create Review'
     click_on 'Great Product'
-    click_on 'Edit or Delete Review'
+    click_button 'Edit or Delete Review'
     fill_in 'Enter a title for your review:', :with => ''
     fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => ''
     fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => ''
-    click_on 'Update Review'
+    click_button 'Update Review'
     expect(page).not_to have_content "Review successfully updated!"
   end
 end
@@ -86,21 +86,21 @@ end
     it "deletes a review" do
       visit root_path
       click_link 'Head to our product catalogue.'
-      click_link 'Add a new product'
+      click_button 'Add a new product'
       fill_in 'Product Name', :with => 'spicy beans'
       fill_in 'Product Price', :with => '3.50'
       fill_in 'Product Origin', :with => 'Brazil'
-      click_on 'Create Product'
+      click_button 'Create Product'
       click_on 'Spicy Beans'
-      click_on 'Add a review for this product'
+      click_button 'Add a review for this product'
       fill_in 'Enter your name:', :with => 'Sally Robinson'
       fill_in 'Enter a title for your review:', :with => 'Great Product'
       fill_in 'Enter your review (Must be between 50 & 250 characters):', :with => 'this is a lot of characterssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
       fill_in 'Enter a rating for the product (1 to 5, 5 being the highest):', :with => '1'
-      click_on 'Create Review'
+      click_button 'Create Review'
       click_on 'Great Product'
-      click_on 'Edit or Delete Review'
-      click_on 'Delete Review'
+      click_button 'Edit or Delete Review'
+      click_link 'Delete Review'
       expect(page).not_to have_content "Great Product"
     end
   end
