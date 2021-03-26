cfc665a9bcdbcdfe40b05c56ca8b6e6948508d56
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-26 13:38:50 -0700

    Add most reviewed scope and adjusted formatting

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,13 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(7)
+  )}
 
   before_save(:titleize_product)
   
