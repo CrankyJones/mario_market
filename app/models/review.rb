9dc99793fcd9577f6e35c85027c83478b8724f0a
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-03-26 12:08:27 -0700

    Work on getting some scopes for the home page

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
   validates :author, presence: true 
   validates_length_of  :content_body, minimum: 50, maximum: 250
   validates_inclusion_of :rating, in: 1..5
+
+  scope :recent_reviews, -> { order(created_at: :desc).limit(7)}
 end
